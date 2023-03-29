0bc004c65c830f16c3501d8b1a797ccb6f51804b
GooseMooz <goosev.goose@gmail.com>
2023-03-28 20:21:23 -0700

    Scripting tests

--- scripts/background.js
+++
@@ -1,5 +0,0 @@
-function changeColorTest(color) {
-    chrome.tabs.executeScript({
-        code: `document.body.style.backgroundColor = "${color}";`
-    }).then(r => console.log(r));
-}
--- scripts/popup_animation.js
+++ scripts/popup_animation.js
@@ -4,6 +4,10 @@ const body = document.querySelector("body")
 checkbox.addEventListener('change', function () {
     if (checkbox.checked) {
         body.classList.add('dark-mode');
+        chrome.scripting.executeScript({
+            target: {tabId: tab.id, allFrames: true},
+            files: ['scripts/background.js']
+        }).then(r => console.log(r));
     }
     else {
         body.classList.remove('dark-mode');
