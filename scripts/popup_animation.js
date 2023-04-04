d1c032581173e5aa629798a1bbc45a8aa5f620fb
GooseMooz <goosev.goose@gmail.com>
2023-04-04 00:27:28 -0700

    Scripting

--- scripts/popup_animation.js
+++ scripts/popup_animation.js
@@ -2,26 +2,31 @@ const checkbox = document.querySelector("input[type=checkbox]")
 const body = document.querySelector("body")
 
 function changeBGColor () {
-    const color = document.defaultView.getComputedStyle(document.querySelector("body"), null).backgroundColor
     let nodes = document.body.getElementsByTagName("*")
     for (let i = 0; i < nodes.length; i++) {
+        console.log([nodes[i]])
         console.log(document.defaultView.getComputedStyle(nodes[i], null).backgroundColor)
-    }
-    if (/^rgb\(\s*\d+,\s*\d+,\s*\d+\)$/gm.test(color)) {
-        let r = parseInt(color.match(/\d+/g)[0]);
-        let g = parseInt(color.match(/\d+/g)[1]);
-        let b = parseInt(color.match(/\d+/g)[2]);
+        console.log("==================")
+        const color = document.defaultView.getComputedStyle(nodes[i], null).backgroundColor
+        if (/^rgb\(\s*\d+,\s*\d+,\s*\d+\)$/gm.test(color) || /^rgba\(\s*\d+,\s*\d+,\s*\d+\S+\s*\S+$/gm.test(color)) {
+            console.log("NE JOPA")
+            console.log(color.match(/\d+/g))
+            let r = parseInt(color.match(/\d+/g)[0]);
+            let g = parseInt(color.match(/\d+/g)[1]);
+            let b = parseInt(color.match(/\d+/g)[2]);
 
-        r = 255 - r
-        g = 255 - g
-        b = 255 - b
+            r = 255 - r
+            g = 255 - g
+            b = 255 - b
 
-        let new_col = '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)
-        document.body.style.transition = "background-color 0.4s ease"
-        document.body.style.backgroundColor = new_col
-    }
-    else {
-        document.body.style.backgroundColor = "#FFFFFF"
+            let new_col = '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)
+            nodes[i].style.transition = "background-color 0.4s ease"
+            nodes[i].style.setProperty('background', new_col, 'important')
+        }
+        else {
+            console.log('JOPA')
+            document.body.style.backgroundColor = "#FFFFFF"
+        }
     }
 }
 
