055bf696c6ec469cf955ea03f5619118080ceb21
GooseMooz <goosev.goose@gmail.com>
2023-04-14 01:55:20 -0700

    Popup state saving

--- scripts/popup_animation.js
+++ scripts/popup_animation.js
@@ -1,16 +1,28 @@
 const checkbox = document.querySelector("input[type=checkbox]")
 const body = document.querySelector("body")
 
-// checkbox.addEventListener('change', async function () {
-//     await chrome.storage.local.set({ state: checkbox.checked }).then(() => {
-//         console.log("Value is set to " + checkbox.checked);
-//     });
-// });
-//
-// chrome.storage.local.get(['state'], function(result) {
-//     document.getElementById('checkbox').checked = result.state;
-//     console.log("Value currently is: " + result.state)
-// });
+checkbox.addEventListener('change', async function () {
+    await chrome.storage.local.set({ state: checkbox.checked }).then(() => {
+        console.log("Value is set to " + checkbox.checked);
+    });
+});
+
+chrome.storage.local.get(['state'], async function (result) {
+    document.getElementById('checkbox').checked = result.state;
+    if (checkbox.checked) {
+        body.classList.add('dark-mode');
+        let queryOptions = {active: true, lastFocusedWindow: true};
+        let [tab] = await chrome.tabs.query(queryOptions);
+        chrome.scripting.executeScript({
+            target: {tabId: tab.id},
+            files: ["scripts/backgroundOn.js"],
+        }).then(r => console.log('Black' + r));
+    } else {
+        body.classList.remove('dark-mode');
+    }
+    console.log("Value currently is: " + result.state)
+});
+
 
 checkbox.addEventListener('change', async function () {
     if (checkbox.checked) {
